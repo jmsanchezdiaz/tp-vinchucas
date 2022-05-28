554d3ca038e763113efc2da3d5cd51c7fbfb93ca
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-05-27 23:17:53 -0300

    modify: AplicacionWeb Class

--- tp-vinchucas/src/ar/unq/tpfinal/AplicacionWeb.java
+++ tp-vinchucas/src/ar/unq/tpfinal/AplicacionWeb.java
@@ -1,15 +1,29 @@
 package ar.unq.tpfinal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AplicacionWeb {
 	static private AplicacionWeb app = null;
+	private List<Muestra> muestras;
 	
-	private AplicacionWeb() {}
+	private AplicacionWeb() {
+		muestras  = new ArrayList<Muestra>();
+	}
 	
 	public static AplicacionWeb getAplicacionWeb() {
 		if(app == null) {
 			app = new AplicacionWeb();
 		}
 		return app;
+	}
+
+	public List<Muestra> getMuestras() {
+		return muestras;
+	}
+
+	public void setMuestras(List<Muestra> muestras) {
+		this.muestras = muestras;
 	}
 	
 }
