435b84d0c683a1edabb91c13aa12bd74e8e2c5e1
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-06-01 00:18:11 -0300

    fixed errors

--- tp-vinchucas/src/ar/unq/tpfinal/usuario/Usuario.java
+++ tp-vinchucas/src/ar/unq/tpfinal/usuario/Usuario.java
@@ -11,11 +11,10 @@ import ar.unq.tpfinal.ubicacion.Ubicacion;
 
 /**
  * <p>
- * Clase abstracta que representa al concepto de un usuario, definiendo su
- * comportamiento por defecto y el q puede ser extendido
+ * 	Clase abstracta que representa al concepto de un usuario,
+ *  definiendo su comportamiento por defecto y el q puede ser extendido
  * 
  * </p>
- * 
  * @author juan manuel sanchez diaz
  *
  */
@@ -24,27 +23,27 @@ public abstract class Usuario {
 	private String nombre;
 	private String id;
 	private NivelDeConocimiento nivelDeConocimiento;
-
+	
 	public void enviarMuestra(AplicacionWeb app, Ubicacion ubi, Foto foto, EspecieVinchuca especie) {
 		app.agregarMuestra(new Muestra(this, ubi, foto, especie));
 	}
-
+	
 	public void opinarMuestra(AplicacionWeb app, Muestra muestra, Opiniones opinion) throws Exception {
 		app.agregarOpinionA(muestra, new Opinion(this, opinion));
 	}
-
+	
 	public String getNombre() {
 		return nombre;
 	}
-
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	
 	public String getId() {
 		return id;
 	}
-
+	
 	public void setId(String id) {
 		this.id = id;
 	}
@@ -53,22 +52,11 @@ public abstract class Usuario {
 		return nivelDeConocimiento;
 	}
 
-<<<<<<< HEAD
-
-=======
 	public void setNivelDeConocimiento(NivelDeConocimiento nivelDeConocimiento) {
 		this.nivelDeConocimiento = nivelDeConocimiento;
 	};
-	
->>>>>>> refs/remotes/origin/master
-	@Override
-	public boolean equals(Object obj) {
-		Usuario otherUser = (Usuario) obj;
-		return this.getId().equals(otherUser.getId());
-	}
 
 	public abstract void subirDeNivel();
-
 	public abstract void bajarDeNivel();
 
 }
