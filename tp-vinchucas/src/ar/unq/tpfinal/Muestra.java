f2c8f9ad3f41cd2e34fb6b835bfad9d6cee69401
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-06-11 19:33:58 -0300

    add: Comments in Muestra

--- tp-vinchucas/src/ar/unq/tpfinal/Muestra.java
+++ tp-vinchucas/src/ar/unq/tpfinal/Muestra.java
@@ -123,6 +123,11 @@ public class Muestra {
 		return getResultadoActual() == valorBuscado;
 	}
 
+	/**
+	 * Indica si la muestra fue opinada por el usuario pasado por parametros.
+	 * @param usuario
+	 * @return boolean
+	 */
 	public boolean fueVotadaEn(LocalDate fecha) {
 		return this.getOpiniones().stream().anyMatch(opinion -> opinion.getFechaCreacion().equals(fecha));
 	}
@@ -147,10 +152,19 @@ public class Muestra {
 		return getFechaCreacion().isBefore(fechaInicio) && getFechaCreacion().isAfter(fechaFin);
 	}
 
+	/**
+	 * Indica si la muestra fue enviada por el usuario pasado por parametros.
+	 * @param usuario
+	 * @return boolean
+	 */
 	public boolean fueEnviadaPor(Usuario usuario) {
 		return this.getUsuario().equals(usuario);
 	}
 
+	/**
+	 * Si la muestra esta verificada, notifica a las zonas pasadas por parametro.
+	 * @param {List<ZonaDeCobertura>} zonasDeLaMuestra
+	 */
 	public void notificarValidacionSiCorresponde(List<ZonaDeCobertura> zonasDeLaMuestra) {
 		if (this.esMuestraVerificada()) {
 			zonasDeLaMuestra.forEach(zona -> zona.notificar(this, Aspecto.MUESTRA_VERIFICADA));
